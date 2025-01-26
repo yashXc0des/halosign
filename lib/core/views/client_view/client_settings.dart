6eabc1e27911d47f8166809af8612129de5715e3
Alphasquare1 <yashXcodes@gmail.com>
2025-01-26 16:19:29 +0530

    aggrement add krne ka option and aggrement dsiplay krne ka option

--- lib/core/views/client_view/client_settings.dart
+++ lib/core/views/client_view/client_settings.dart
@@ -1,15 +1,24 @@
-import 'package:flutter/cupertino.dart';
+import 'package:esign/core/views/signup_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../providers/auth_provider.dart';
 
-class Client_SettingsScreen extends StatefulWidget {
-  const Client_SettingsScreen({super.key});
-
+class Client_SettingsScreen extends ConsumerWidget {
   @override
-  State<Client_SettingsScreen> createState() => _Client_SettingsScreenState();
-}
-
-class _Client_SettingsScreenState extends State<Client_SettingsScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            // Sign out the user using Riverpod
+            await ref.read(authProvider).signOut(); // Use the provider variable directly
+            // Navigate back to the sign-in screen
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => signup_view()));// Update with your sign-in route
+          },
+          child: Text('Sign Out'),
+        ),
+      ),
+    );
   }
 }
